b834ddb6706a521c21e828da5ce133bb1afad77e
Percevent13 <fccharlebois@hotmail.com>
2024-03-06 08:59:00 -0500

    Added Event subscription to TooltipManager

--- Runtime/Scripts/TooltipManager.cs
+++ Runtime/Scripts/TooltipManager.cs
@@ -7,16 +7,37 @@ namespace jeanf.tooltip
 {
     public class TooltipManager : MonoBehaviour
     {
+        [Header("Listening On")]
+        [SerializeField] StringEventChannelSO tooltipListener;
+
         [Header("Broadcasting On")]
-        [SerializeField] StringEventChannelSO stringEventChannelSO;
+        [SerializeField] StringEventChannelSO tooltipSender;
         private bool DetectHMDStatus()
         {
             return true;
         }
 
+
+        private void OnEnable()
+        {
+            tooltipListener.OnEventRaised += value => SendTooltip(value);
+        }
+        private void OnDisable() => Unsubscribe(); 
+        private void OnDestroy() => Unsubscribe();
+
+        private void Unsubscribe()
+        {
+            tooltipListener.OnEventRaised -= value => SendTooltip(value);
+        }
+
         public void SendTooltip(TooltipSO tooltipSO)
         {
-            stringEventChannelSO.RaiseEvent(tooltipSO.Tooltip);
+            tooltipSender.RaiseEvent(tooltipSO.Tooltip);
+        }
+
+        public void SendTooltip(string tooltip)
+        {
+            tooltipSender.RaiseEvent(tooltip);
         }
     }
 }
